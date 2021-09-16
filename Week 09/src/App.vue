e71cc972ddd123af718a6ab7100032b6336c56de
Sim <sim@snorl.ax>
2021-09-16 19:24:18 +0800

    update menu

--- Week 09/src/App.vue
+++ Week 09/src/App.vue
@@ -3,10 +3,6 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 
-const activeIndex = '1'
-function handleSelect(key, keyPath) {
-  console.log(key, keyPath)
-}
 </script>
 
 <template>
@@ -14,23 +10,10 @@ function handleSelect(key, keyPath) {
     :default-active="activeIndex"
     class="el-menu-demo"
     mode="horizontal"
-    @select="handleSelect"
+    :router="true"
   >
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item index="/demo1">json 2 form</el-menu-item>
+    <el-menu-item index="/demo2">form dragging</el-menu-item>
   </el-menu>
   <div class='bottom'>
     <router-view/>
